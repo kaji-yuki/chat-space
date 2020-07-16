bceae944de456eb96baddec3da57e6cf98efb52a
kaji-yuki <k.yukiko.a@gmail.com>
2020-07-16 12:09:28 +0900

    implemented new_group

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root  "messages#index"
-  resources :users, only: [:update, :edit]
-  resources :groups,only: [:new, :create]
+  resources :users, only: [:new, :update, :edit]
+  resources :groups, only: [:new, :create]
 end
