48734b719f1ec0179cd99993593840265e5e45cb
kaji-yuki <k.yukiko.a@gmail.com>
2020-08-07 16:44:55 +0900

    before fix

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -17,7 +17,18 @@ $(function(){
                 </div>
                 `;
     $("#UserSearchResult").append(html);
-}
+  }
+
+  function addMember(addUserId,addUserName){
+    let html = `
+                <div class="ChatMember">
+                  <p class="ChatMember__name">${addUserName}</p>
+                  <input name="group[user_ids][]" type="hidden" value="${addUserId}" />
+                  <div class="ChatMember__remove ChatMember__button">削除</div>
+                  </div>
+                  `;
+    $(".ChatMembers").append(html)
+  }
 
   $("#UserSearch__field").on("keyup", function(){
     let input = $("#UserSearch__field").val();
@@ -43,4 +54,16 @@ $(function(){
       alert("ユーザー検索に失敗しました");
     });
   });
+
+  $("#UserSearchResult").on("click",".ChatMember__add",function(){
+    const addUserId = $(this).attr("data-user-id") ;
+    const addUserName = $(this).attr("data-user-name");
+    $(this).parent().remove();
+    addMember(addUserId, addUserName);
+  });
+  
+  $(".ChatMembers").on("click", ".ChatMember__remove", function(){
+    $(this).parent().remove();
+  })
+
   });
