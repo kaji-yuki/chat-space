7523cac7f2c6f498f0fc66aedfcdf5efed73fbd0
kaji-yuki <k.yukiko.a@gmail.com>
2020-08-09 11:26:10 +0900

    implemented autoUpdate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   
+  def new
+  end
+
   def index
     return nil if params[:keyword] == ""
     @users = User.where(["name LIKE ?", "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
